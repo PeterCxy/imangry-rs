49949a601116fa3fee111e150fcf24671d3b5b95
Peter Cai <peter@typeblog.net>
2018-08-31 18:49:01 +0800

    implement life_set for migration

--- backend/src/life.rs
+++ backend/src/life.rs
@@ -1,4 +1,4 @@
-use actix_web::{http, App, AsyncResponder, Responder, HttpRequest, HttpResponse};
+use actix_web::{http, App, AsyncResponder, Form, Responder, HttpRequest, HttpResponse};
 use actix_web::error::ErrorInternalServerError;
 use app::AngryAppState;
 use futures::Future;
@@ -6,6 +6,7 @@ use futures::Future;
 pub fn setup_routes(app: App<AngryAppState>) -> App<AngryAppState> {
     app.resource("/p/life", |r| r.method(http::Method::POST).f(life_add))
         .resource("/l/life", |r| r.method(http::Method::GET).f(life_show))
+        .resource("/p/life_set", |r| r.method(http::Method::POST).with(life_set))
 }
 
 fn life_show(req: &HttpRequest<AngryAppState>) -> impl Responder {
@@ -33,6 +34,26 @@ fn life_add(req: &HttpRequest<AngryAppState>) -> impl Responder {
             state.spawn_pool(db.set_async_u64("life_secs", new_a))
                 .and_then(move |_| Ok(HttpResponse::Ok().body(format!("{}", new_a))))
         })
+        .map_err(|e| {
+            // TODO: Properly return the error
+            println!("{:?}", e);
+            ErrorInternalServerError(e)
+        })
+        .responder()
+}
+
+#[derive(Deserialize)]
+struct SetLifeParams {
+    life: u64
+}
+
+// ONLY FOR INTERNAL PURPOSES
+// THIS SHOULD NOT BE EXPOSED TO THE OUTSIDE OF THE REVERSE PROXY
+fn life_set((req, params): (HttpRequest<AngryAppState>, Form<SetLifeParams>)) -> impl Responder {
+    let state = req.state().clone();
+    let db = state.get_db();
+    state.spawn_pool(db.set_async_u64("life_secs", params.life))
+        .map(|_| HttpResponse::Ok())
         .map_err(|e| {
             // TODO: Properly return the error
             println!("{:?}", e);
